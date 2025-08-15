2dff195288ab56aae40a87d674ef2d392bafc387
Afriawan <apri49691@gmail.com>
2025-08-15 18:34:20 +0700

    feat(tv-series) show all episodes & seasons

---
+++ app/episode/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Page() {
+    return (
+        <div className="flex w-full h-screen justify-center items-center font-semibold text-2xl">
+            Page not found
+        </div>
+    )
+}
