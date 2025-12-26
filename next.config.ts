b9b5d6a86d3b4c33f56d5bab7117865642c5e343
Afriawan <apri49691@gmail.com>
2025-12-26 08:30:28 +0700

    Adds X-Frame-Options and implements headers [CSP]

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,23 @@
 import type { NextConfig } from "next";
-import { headers } from "next/headers";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  return: [
-    {
-      source: "/:path*",
-      headers: [
-        {
-          key: "Content-Security-Policy",
-          value: "frame-ancestors 'self'; navigate-to 'self';",
-        },
-      ],
-    },
-  ],
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self'; navigate-to 'self';",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
