77e27fde9fb173418c616b6c0c857b6af3aec8a1
Afriawan Maulana <114582310+AfriawanMaulana@users.noreply.github.com>
2026-01-02 06:57:59 +0700

    Update page.tsx

--- app/movie/page.tsx
+++ app/movie/page.tsx
@@ -1,17 +1,13 @@
 import MovieList from "../components/MovieList";
 import Navbar from "../components/Navbar";
-import MovieSkeleton from "../components/MovieSkeleton";
-import { Suspense } from "react";
 
 export default function Page() {
     return (
         <div className="flex flex-col">
             <title>Movies - TERFLIX</title>
             <Navbar />
-            <section className="px-5 lg:px-14 py-20">
-                <Suspense fallback={<MovieSkeleton />}>
-                    <MovieList API_URL="/api/tmdb/discover/movie" filterCountry="" category="movie" header="popular" isPagination />
-                </Suspense>
+            <section className="px-5 lg:px-14 py-20">    
+                <MovieList API_URL="/api/tmdb/discover/movie" filterCountry="" category="movie" header="popular" isPagination />
             </section>
         </div>
     )
