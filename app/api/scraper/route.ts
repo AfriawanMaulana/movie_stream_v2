ccf2ad5c8744e1bdf3d92894db618bf4b9b5b8fe
Afriawan <apri49691@gmail.com>
2026-01-02 12:15:00 +0700

    try to fix countries page v3

--- app/api/scraper/route.ts
+++ app/api/scraper/route.ts
@@ -1,17 +1,20 @@
-// import { NextRequest, NextResponse } from "next/server";
-// import puppeteer from "puppeteer";
+import { NextResponse } from "next/server";
+import puppeteer from "puppeteer";
 
-// export async function GET(req: NextRequest) {
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
+export async function GET() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-//   await page.goto('https://tv6.lk21official.cc/jurassic-world-rebirth-2025');
-//   await page.waitForSelector('#loadPlayer iframe');
+  await page.goto("https://tv6.lk21official.cc/jurassic-world-rebirth-2025");
+  await page.waitForSelector("#loadPlayer iframe");
 
-//   const pageTitle = await page.title();
-//   const iframeSrc = await page.$eval('#loadPlayer iframe', el => el.src);
+  const pageTitle = await page.title();
+  const iframeSrc = await page.$eval("#loadPlayer iframe", (el) => el.src);
 
-//   await browser.close();
+  await browser.close();
 
-//   return NextResponse.json({ title: pageTitle, iframeSrc: iframeSrc }, { status: 200 });
-// }
+  return NextResponse.json(
+    { title: pageTitle, iframeSrc: iframeSrc },
+    { status: 200 }
+  );
+}
