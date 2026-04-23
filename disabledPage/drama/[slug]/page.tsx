e429e1c05c2a08b2e03d205b208f8b240b1352d0
Afriawan <apri49691@gmail.com>
2026-04-23 09:09:34 +0700

    disabled page

--- disabledPage/drama/[slug]/page.tsx
+++ disabledPage/drama/[slug]/page.tsx
@@ -1,12 +1,12 @@
-import DramaDetail from "../components/DramaDetail";
+// import DramaDetail from "../components/DramaDetail";
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: Promise<{ id: string; ep?: string }>;
-}) {
-  const params = await searchParams;
-  const dramaId = params.id;
-  const episodeId = params.ep;
-  return <DramaDetail id={`${dramaId}`} ep={`${episodeId}`} />;
-}
+// export default async function Page({
+//   searchParams,
+// }: {
+//   searchParams: Promise<{ id: string; ep?: string }>;
+// }) {
+//   const params = await searchParams;
+//   const dramaId = params.id;
+//   const episodeId = params.ep;
+//   return <DramaDetail id={`${dramaId}`} ep={`${episodeId}`} />;
+// }
